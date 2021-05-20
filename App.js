8f932789ef08ed743e0419485656effd35287fca
Hinako-Tanaka <youngbird1228@gmail.com>
2021-05-20 17:59:41 +0900

    set

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ドラマの名言で覚える韓国語</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={{ backgroundColor: 'blue' }}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>始める</Text>
+      </TouchableOpacity>
     </View>
   );
 }
