ea39ec2561fa5d0884208e93470cf99773cbdce0
Hinako-Tanaka <youngbird1228@gmail.com>
2021-05-29 22:50:20 +0900

    set react-native-elements

--- screens/UnitSelectionScreen.js
+++ screens/UnitSelectionScreen.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-// import { Header, Button } from 'react-native-elements';
+import { Header, Button, Icon } from 'react-native-elements';
 
 export default class UnitSelectionScreen extends Component {
     render() {
       return (
         <>
           <Header
-            leftComponent={{ icon: 'menu', color: '#fff' }}
-            centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+            leftComponent={{ icon: 'chevron-left', color: '#fff' }}
+            centerComponent={{ text: 'ドラマの名言で覚える韓国語', style: { color: '#fff' } }}
             rightComponent={{ icon: 'home', color: '#fff' }}
           />
           <Button 
@@ -17,12 +17,7 @@ export default class UnitSelectionScreen extends Component {
               this.props.navigation.navigate('Question')
             }}
            />
-          <Button 
-            title="2020年" 
-            onPress={() => {
-              this.props.navigation.navigate('Question')
-            }}
-          />
+          
         </>
       );
 
