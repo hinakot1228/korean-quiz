49721dfa15930bf1a34d2eaaac2315d374d3e551
Hinako-Tanaka <youngbird1228@gmail.com>
2021-06-06 23:02:08 +0900

    modify files

--- screens/questionScreen.js
+++ screens/questionScreen.js
@@ -1,17 +1,11 @@
 import React, { Component, useState } from 'react';
-import { render } from 'react-dom';
 import { View } from 'react-native';
-import { ThemeProvider, Card, Header, Button, Text, Overlay} from 'react-native-elements';
-// import {
-//   MatchingQuestion,
-//   MultipleChoiceQuestion,
-//   QuizContainer,
-//   WritingQuestion,
-// } from 'react-native-quiz-maker';
+import { ThemeProvider, Card, Header, Button, Text } from 'react-native-elements';
 
-export default function QuestionScreen() {
+export default function QuestionScreen( {navigation} ) {
   const questions = [
     {
+      language: '韓国語',
       questionText: '俺の価値をお前が決めるな。',
 			answerOptions: [
 				{ answerText: '네 가치를 내가 정하지 마.', isCorrect: false },
@@ -19,16 +13,58 @@ export default function QuestionScreen() {
 				{ answerText: '내 같이를 네가 정하지 마.', isCorrect: false },
 				{ answerText: '내 가치를 네가 정해라.', isCorrect: false },
 			],
-    }
+    },
+    {
+      language: '日本語',
+      questionText: '살아만 있다면 뭐든 별게 아니야.',
+			answerOptions: [
+				{ answerText: '生きてさえいれば何だってたいしたことない', isCorrect: true },
+				{ answerText: '愛してさえいれば何だってたいしたことない', isCorrect: false },
+				{ answerText: '生きてさえいれば何だって星ではない', isCorrect: false },
+				{ answerText: '生きていれば何だってたいしたことない', isCorrect: false },
+			],
+    },
+    {
+      language: '韓国語',
+      questionText: '望んだとおりに全てを手に入れる。それがまさに僕の夢だから',
+			answerOptions: [
+				{ answerText: '원하는 만큼 다 가질 거야. 그게 바로 내 꿈일 테니까', isCorrect: false },
+				{ answerText: '원하는 대로 다 가질 거야. 그게 바로 내 꼼일 테니까', isCorrect: false },
+				{ answerText: '원하는 대로 다 가질 거야. 그게 바로 내 꿈일 테니까', isCorrect: true },
+				{ answerText: '원하는 대로 다 가지을 거야. 그게 바로 내 꿈이을 테니까', isCorrect: false },
+			],
+    },
+    {
+      language: '日本語',
+      questionText: '제가 생각하는 강함은 사람에게서 나옵니다.',
+			answerOptions: [
+				{ answerText: '僕は考える経験は人から出てきます。', isCorrect: false },
+				{ answerText: '僕は考える強い人から出てきます。', isCorrect: false },
+				{ answerText: '僕は考える強さは愛から出てきます。', isCorrect: false },
+				{ answerText: '僕は考える強さは人から出てきます。', isCorrect: true },
+			],
+    },
+    {
+      language: '日本語',
+      questionText: '깊은 어둠에 가둬봐라 나는 홀로 빛나는 돌덩이',
+			answerOptions: [
+				{ answerText: '甲は闇に閉じこめておけ 私は一人で輝く石ころ', isCorrect: false },
+				{ answerText: '深い闇に閉じこめておけ 私は一人で借金が出る石ころ', isCorrect: false },
+				{ answerText: '深い闇に行け 私は一人で輝く石ころ', isCorrect: false },
+				{ answerText: '深い闇に閉じこめておけ 私は一人で輝く石ころ', isCorrect: true },
+			],
+    },
   ]
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] =useState(0);
 
+
   const handleAnswerOptionClick = (isCorrect) => {
     if (isCorrect) {
       setScore(score + 1);
+
     }
 
     const nextQuestion = currentQuestion + 1;
@@ -41,7 +77,7 @@ export default function QuestionScreen() {
 
   return (
     <View>
-      {/* <ThemeProvider theme={theme}> */}
+      <ThemeProvider theme={theme}>
         <Header
           containerStyle={{ position: 'absolute', top: 0 }}
           // leftComponent={{ 
@@ -55,21 +91,21 @@ export default function QuestionScreen() {
           rightComponent={{ 
             icon: 'home', 
             color: '#fff',
-            onPress: () => {
-              this.props.navigation.navigate('Start')
-            },
+            onPress: () => navigation.navigate('Start')
           }}
         />
         <Card containerStyle={{top: 70, height:'50%'}}>
           <Card.Title>Q.{currentQuestion + 1}</Card.Title>
           <Card.Divider/>
           <Text h5 style={{paddingBottom: 10}}>
-            {/* 以下のセリフを{this.props.language}にしてみよう。 */}
+            以下のセリフを{questions[currentQuestion].language}にしてみよう。
           </Text>
-          <Text h4>問題：{questions[0].questionText}</Text>
+          <Text h4 style={{textAlign: 'center'}}>
+            ❝{questions[currentQuestion].questionText}❞
+          </Text>
         </Card>
-        <View style={{top: '20%' }} >
-          {questions[0].answerOptions.map((answerOption) => (
+        <View style={{top: '16%' }} >
+          {questions[currentQuestion].answerOptions.map((answerOption) => (
             <Button
               title={answerOption.answerText}
               onPress={
@@ -81,31 +117,11 @@ export default function QuestionScreen() {
               
             />
           ))}
-          
-          {/* <Button
-            title={this.props.answerSentence}
-            // onClick={}
-          />
-          <Button
-            title={this.props.answerSentence}
-            // onClick={}
-          />
-          <Button
-            title={this.props.answerSentence}
-            // onClick={}
-          /> */}
         </View>
-      {/* </ThemeProvider> */}
+      </ThemeProvider>
     </View>
   );
 }
-
-
-
-// export default class QuestionScreen extends Component {
-//   render() {
-
-// }
 
 const theme = {
   Button: {
