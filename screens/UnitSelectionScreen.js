1184b45d5468a5d0973068bb2125c1c8b209bf73
hinakot1228 <74905456+hinakot1228@users.noreply.github.com>
2021-07-15 15:25:30 +0900

    modify header & unitselection buttons

--- screens/UnitSelectionScreen.js
+++ screens/UnitSelectionScreen.js
@@ -23,56 +23,50 @@ export default class UnitSelectionScreen extends Component {
       return (
         <ThemeProvider theme={theme}>
           <View  style={{ backgroundColor: '#FFFFFF', height: '100%'}}>
-          <Header
-            leftComponent={{ 
-              icon: 'chevron-left', 
-              color: '#fff',
-              onPress: () => {
-                this.props.navigation.navigate('Start')
-              },
-             }}
-            centerComponent={{ text: 'ドラマのなかの韓国語', style: { color: '#fff', fontSize:17 } }}
-            rightComponent={{ 
-              icon: 'home', 
-              color: '#fff',
-              onPress: () => {
-                this.props.navigation.navigate('Start')
-              },
-            }}
-          />
-          <View style={{flex: 1, justifyContent: 'center'}}>
-          <Button 
-            title="梨泰院クラス"
-            onPress={() => {
-              this.props.navigation.navigate('ItaewonClass')
-            }}
-           />
-          <Button 
-            title="愛の不時着"
-            onPress={() => {
-              this.props.navigation.navigate('CrashLandingOnYou')
-            }}
-           />
-          <Button 
-            title="スタートアップ"
-            onPress={() => {
-              this.props.navigation.navigate('StartUp')
-            }}
-           />
-          <Button 
-            title="賢い医師生活"
-            onPress={() => {
-              this.props.navigation.navigate('HospitalPlaylist')
-            }}
-           />
-          <Button 
-            title="椿の花咲く頃"
-            onPress={() => {
-              this.props.navigation.navigate('WhenTheCamelliaBlooms')
-            }}
-           />
-           </View>
-           </View>
+            <View style={{flex: 1}}>
+              <Header
+                containerStyle={{ width: '100%' ,height: '100%',backgroundColor: '#ffb6c1',}}
+                centerComponent={{ text: 'ドラマのなかの韓国語', style: { color: '#fff', fontSize:17 } }}
+                rightComponent={{ 
+                  icon: 'home', 
+                  color: '#fff',
+                  onPress: () => navigation.navigate('Start')
+                }}
+              />
+            </View>
+            <View style={{flex: 10, justifyContent: 'space-around'}}>
+              <Button 
+                title="梨泰院クラス"
+                onPress={() => {
+                  this.props.navigation.navigate('ItaewonClass')
+                }}
+              />
+              <Button 
+                title="愛の不時着"
+                onPress={() => {
+                  this.props.navigation.navigate('CrashLandingOnYou')
+                }}
+              />
+              <Button 
+                title="スタートアップ"
+                onPress={() => {
+                  this.props.navigation.navigate('StartUp')
+                }}
+              />
+              <Button 
+                title="賢い医師生活"
+                onPress={() => {
+                  this.props.navigation.navigate('HospitalPlaylist')
+                }}
+              />
+              <Button 
+                title="椿の花咲く頃"
+                onPress={() => {
+                  this.props.navigation.navigate('WhenTheCamelliaBlooms')
+                }}
+              />
+            </View>
+          </View>
         </ThemeProvider>
       );
     }
